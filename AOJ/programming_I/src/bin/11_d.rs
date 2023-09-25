2a4987ef448c968523d8c60b0e7d0f6d68f8da7d
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2023-09-25 17:18:57 +0900

    OK

--- AOJ/programming_I/src/bin/11_d.rs
+++ AOJ/programming_I/src/bin/11_d.rs
@@ -1,249 +1,172 @@
+use std::cmp::PartialEq;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum DiceRoll {
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
+enum DiceLabel {
+    U, D, R, L, F, B,
 }
 
-impl DiceRoll {
-    fn rev(&self) -> DiceRoll {
+// relation between labels
+impl DiceLabel {
+    fn all() -> Vec<Self> {
+        vec![
+            DiceLabel::U, DiceLabel::D, DiceLabel::R, DiceLabel::L, DiceLabel::F, DiceLabel::B,
+        ]
+    }
+    fn rev(self) -> DiceLabel {
         match self {
-            DiceRoll::R1 => Self::R6,
-            DiceRoll::R2 => Self::R5,
-            DiceRoll::R3 => Self::R4,
-            DiceRoll::R4 => Self::R3,
-            DiceRoll::R5 => Self::R2,
-            DiceRoll::R6 => Self::R1,
+            DiceLabel::U => Self::D,
+            DiceLabel::D => Self::U,
+            DiceLabel::R => Self::L,
+            DiceLabel::L => Self::R,
+            DiceLabel::F => Self::B,
+            DiceLabel::B => Self::F,
         }
     }
-    fn right_hand_around(&self) -> Vec<DiceRoll> {
+    fn right_hand_around(self) -> Vec<DiceLabel> {
         match self {
-            DiceRoll::R1 => vec![DiceRoll::R4, DiceRoll::R2, DiceRoll::R3, DiceRoll::R5],
-            DiceRoll::R2 => vec![DiceRoll::R1, DiceRoll::R4, DiceRoll::R6, DiceRoll::R3],
-            DiceRoll::R3 => vec![DiceRoll::R1, DiceRoll::R2, DiceRoll::R6, DiceRoll::R5],
-            DiceRoll::R4 => vec![DiceRoll::R1, DiceRoll::R5, DiceRoll::R6, DiceRoll::R2],
-            DiceRoll::R5 => vec![DiceRoll::R1, DiceRoll::R3, DiceRoll::R6, DiceRoll::R4],
-            DiceRoll::R6 => vec![DiceRoll::R2, DiceRoll::R4, DiceRoll::R5, DiceRoll::R3],
-            _ => unreachable!(),
+            DiceLabel::U => vec![DiceLabel::F, DiceLabel::R, DiceLabel::B, DiceLabel::L],
+            DiceLabel::D => vec![DiceLabel::F, DiceLabel::L, DiceLabel::B, DiceLabel::R],
+            DiceLabel::R => vec![DiceLabel::U, DiceLabel::F, DiceLabel::D, DiceLabel::B],
+            DiceLabel::L => vec![DiceLabel::U, DiceLabel::B, DiceLabel::D, DiceLabel::F],
+            DiceLabel::F => vec![DiceLabel::U, DiceLabel::L, DiceLabel::D, DiceLabel::R],
+            DiceLabel::B => vec![DiceLabel::U, DiceLabel::R, DiceLabel::D, DiceLabel::L],
+        }
+    }
+    fn into_usize(self) -> usize {
+        match self {
+            DiceLabel::U => 0,
+            DiceLabel::F => 1,
+            DiceLabel::R => 2,
+            DiceLabel::L => 3,
+            DiceLabel::B => 4,
+            DiceLabel::D => 5,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 struct Location {
-    up: DiceRoll,
-    south: DiceRoll,
+    up: DiceLabel,
+    south: DiceLabel,
 }
 
 impl Default for Location {
     fn default() -> Self {
         Self {
-            up: DiceRoll::R1,
-            south: DiceRoll::R2,
+            up: DiceLabel::U,
+            south: DiceLabel::F,
         }
     }
 }
 
 impl Location {
-    fn up(&self) -> DiceRoll {
-        self.up
+    fn new(up: DiceLabel, south: DiceLabel) -> Location {
+        Self { up, south }
     }
-    fn down(&self) -> DiceRoll {
-        self.up().rev()
-    }
-    fn south(&self) -> DiceRoll {
-        self.south
-    }
-    fn north(&self) -> DiceRoll {
-        self.south().rev()
-    }
-    
-    fn spot_by_index(&self, front: DiceRoll, index: usize) -> DiceRoll {
-        let south = self.south();
-        let around = self.up.right_hand_around();
-        let get_front_index: usize = around
-            .iter()
-            .enumerate()
-            .find_map(|(i, &r)| if front == r { Some(i) } else { None })
-            .unwrap();
-        around[(get_front_index + index) % 4]
-    }
-    fn west(&self) -> DiceRoll {
-        self.spot_by_index(self.south(), 3)
-    }
-    fn east(&self) -> DiceRoll {
-        self.spot_by_index(self.south(), 1)
-    }
-    fn spot(&self, dir: Direction) -> DiceRoll {
-        match dir {
-            Direction::S => self.south(),
-            Direction::N => self.north(),
-            Direction::E => self.east(),
-            Direction::W => self.west(),
+    fn get_lotated_label(&self, side: DiceLabel) -> DiceLabel {
+        match side {
+            DiceLabel::U => self.up,
+            DiceLabel::D => self.up.rev(),
+            _ => {
+                let around_usual = DiceLabel::U.right_hand_around();
+                let around_self = self.up.right_hand_around();
+                around_self
+                    .into_iter()
+                    .cycle()
+                    .skip_while(|item| *item != self.south)
+                    .zip(around_usual)
+                    .find_map(|(item1, item2)| if item2 == side { Some(item1) } else { None })
+                    .unwrap()
+            }
         }
     }
-    fn roll_south(&mut self) {
-        let rev_south = self.south().rev();
-        self.south = self.up;
-        self.up = rev_south;
-    }
-    fn roll_north(&mut self) {
-        let rev_up = self.up().rev();
-        self.up = self.south;
-        self.south = rev_up;
-    }
-    fn roll_west(&mut self) {
-        self.up = self.east();
-    }
-    fn roll_east(&mut self) {
-        self.up = self.west();
-    }
-    fn roll(&mut self, dir: Direction) {
-        match dir {
-            Direction::S => self.roll_south(),
-            Direction::N => self.roll_north(),
-            Direction::E => self.roll_east(),
-            Direction::W => self.roll_west(),
-        }
-    }
-    fn roll_as_unmut(&self, dir: Direction) -> Location {
-        let mut other: Location = self.clone();
-        other.roll(dir);
-        other
-    }
-    fn set_by_roll(&mut self, up: DiceRoll, front: DiceRoll) {
-        self.up = up;
-        self.south = front;
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-enum Direction {
-    S,
-    N,
-    E,
-    W,
-}
-
-use std::convert::TryFrom;
-impl TryFrom<&str> for Direction {
-    type Error = ();
-    fn try_from(value: &str) -> Result<Direction, Self::Error> {
-        match value {
-            "S" => Ok(Direction::S),
-            "N" => Ok(Direction::N),
-            "E" => Ok(Direction::E),
-            "W" => Ok(Direction::W),
-            _ => Err(()),
+    fn right_hand_spin(self, side: DiceLabel) -> Self {
+        match side {
+            DiceLabel::U => Self {
+                up: self.get_lotated_label(DiceLabel::U),
+                south: self.get_lotated_label(DiceLabel::L),
+            },
+            DiceLabel::D => Self {
+                up: self.get_lotated_label(DiceLabel::U),
+                south: self.get_lotated_label(DiceLabel::R),
+            },
+            DiceLabel::R => Self {
+                up: self.get_lotated_label(DiceLabel::B),
+                south: self.get_lotated_label(DiceLabel::U),
+            },
+            DiceLabel::L => Self {
+                up: self.get_lotated_label(DiceLabel::F),
+                south: self.get_lotated_label(DiceLabel::D),
+            },
+            DiceLabel::F => Self {
+                up: self.get_lotated_label(DiceLabel::R),
+                south: self.get_lotated_label(DiceLabel::F),
+            },
+            DiceLabel::B => Self {
+                up: self.get_lotated_label(DiceLabel::L),
+                south: self.get_lotated_label(DiceLabel::F),
+            },
         }
     }
 }
 
 #[derive(Debug, Clone)]
 struct Dice {
-    label_1: usize,
-    label_2: usize,
-    label_3: usize,
-    label_4: usize,
-    label_5: usize,
-    label_6: usize,
+    vec: Vec<u8>,
+}
+
+impl PartialEq for Dice {
+    fn eq(&self, other: &Self) -> bool {
+        DiceLabel::all()
+        .into_iter()
+        .flat_map(|roll1| {
+            roll1
+                .right_hand_around()
+                .into_iter()
+                .map(move |roll2| (roll1, roll2))
+        })
+        .any(|(up, front)| {
+            let putted_dice1 = PuttedDice::new(self.clone(), Location::new(up, front));
+            let putted_dice2 = PuttedDice::new(other.clone(), Location::default());
+            putted_dice1 == putted_dice2
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PuttedDice {
+    dice: Dice,
     loc: Location,
 }
 
-impl Dice {
-    fn up(&self) -> usize {
-        match self.loc.up() {
-            DiceRoll::R1 => self.label_1,
-            DiceRoll::R2 => self.label_2,
-            DiceRoll::R3 => self.label_3,
-            DiceRoll::R4 => self.label_4,
-            DiceRoll::R5 => self.label_5,
-            DiceRoll::R6 => self.label_6,
-        }
+impl PuttedDice {
+    fn new(dice: Dice, loc: Location) -> PuttedDice {
+        Self { dice, loc }
     }
-    fn down(&self) -> usize {
-        match self.loc.down() {
-            DiceRoll::R1 => self.label_1,
-            DiceRoll::R2 => self.label_2,
-            DiceRoll::R3 => self.label_3,
-            DiceRoll::R4 => self.label_4,
-            DiceRoll::R5 => self.label_5,
-            DiceRoll::R6 => self.label_6,
-        }
+    fn get_by_label(&self, label: DiceLabel) -> u8 {
+        self.dice.vec[self.loc.get_lotated_label(label).into_usize()]
     }
-    fn spot(&self, dir: Direction) -> usize {
-        match self.loc.spot(dir) {
-            DiceRoll::R1 => self.label_1,
-            DiceRoll::R2 => self.label_2,
-            DiceRoll::R3 => self.label_3,
-            DiceRoll::R4 => self.label_4,
-            DiceRoll::R5 => self.label_5,
-            DiceRoll::R6 => self.label_6,
-        }
-    }
-    fn get_from_num(&self, i: usize) -> Option<DiceRoll> {
-        match i {
-            _ if i == self.label_1 => Some(DiceRoll::R1),
-            _ if i == self.label_2 => Some(DiceRoll::R2),
-            _ if i == self.label_3 => Some(DiceRoll::R3),
-            _ if i == self.label_4 => Some(DiceRoll::R4),
-            _ if i == self.label_5 => Some(DiceRoll::R5),
-            _ if i == self.label_6 => Some(DiceRoll::R6),
-            _ => None,
-        }
-    }
-    fn set_by_roll(&mut self, up: DiceRoll, front: DiceRoll) {
-        self.loc.set_by_roll(up, front);
-    }
-    fn roll(&mut self, dir: Direction) {
-        self.loc.roll(dir);
-    }
-    fn roll_as_unmut(&self, dir: Direction) -> Dice {
-        let mut other: Dice = self.clone();
-        other.loc.roll(dir);
-        other
+}
+
+impl PartialEq for PuttedDice {
+    fn eq(&self, other: &Self) -> bool {
+        DiceLabel::all()
+        .into_iter()
+        .all(|label| self.get_by_label(label) == other.get_by_label(label))
     }
 }
 
 fn main() {
     let dices = input_dices();
     let n = dices.len();
-    let exists_eq_dice = (0..n).flat_map(move |i| (i+1..n).map(move |j| (i, j))).any(|(i,j)|{
-        eq(&dices[i], &dices[j])
-    });
+    let exists_eq_dice = (0..n)
+        .flat_map(move |i| (i + 1..n).map(move |j| (i, j)))
+        .any(|(i, j)| dices[i] == dices[j]);
     if !exists_eq_dice {
         println!("Yes");
     } else {
         println!("No");
     }
-}
-
-fn eq(dice1: &Dice, dice2: &Dice) -> bool {
-    let all_combination: Vec<(DiceRoll, DiceRoll)> = vec![
-        DiceRoll::R1,
-        DiceRoll::R2,
-        DiceRoll::R3,
-        DiceRoll::R4,
-        DiceRoll::R5,
-        DiceRoll::R6,
-    ].into_iter().flat_map(|roll1| roll1.right_hand_around().into_iter().map(move |roll2| (roll1, roll2))).collect();
-    all_combination.into_iter().any(|(up, front)| {
-        let mut new_dice1 = dice1.clone();
-        new_dice1.set_by_roll(up, front);
-        eq_as_located(&new_dice1, dice2)
-    })
-}
-
-fn eq_as_located(dice1: &Dice, dice2: &Dice) -> bool {
-    dice1.up() == dice2.up() &&
-    dice1.down() == dice2.down() &&
-    dice1.spot(Direction::S) == dice2.spot(Direction::S) &&
-    dice1.spot(Direction::N) == dice2.spot(Direction::N) &&
-    dice1.spot(Direction::E) == dice2.spot(Direction::E) &&
-    dice1.spot(Direction::W) == dice2.spot(Direction::W) 
 }
 
 fn input_dices() -> Vec<Dice> {
@@ -252,23 +175,38 @@ fn input_dices() -> Vec<Dice> {
 }
 
 fn input_dice() -> Dice {
-    let v: Vec<_> = readline()
+    let vec: Vec<_> = readline()
         .split_whitespace()
-        .map(|str| str.parse::<usize>().unwrap())
+        .map(|str| str.parse::<u8>().unwrap())
         .collect();
-    Dice {
-        label_1: v[0],
-        label_2: v[1],
-        label_3: v[2],
-        label_4: v[3],
-        label_5: v[4],
-        label_6: v[5],
-        loc: Location::default(),
-    }
+    Dice { vec }
 }
 
 fn readline() -> String {
     let mut string = String::new();
     std::io::stdin().read_line(&mut string).unwrap();
     string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn loc_test() {
+        let loc = Location::default();
+        assert_eq!(loc.get_lotated_label(DiceLabel::U), DiceLabel::U);
+        assert_eq!(loc.get_lotated_label(DiceLabel::D), DiceLabel::D);
+        assert_eq!(loc.get_lotated_label(DiceLabel::R), DiceLabel::R);
+        assert_eq!(loc.get_lotated_label(DiceLabel::L), DiceLabel::L);
+
+        let loc = Location::new(DiceLabel::F, DiceLabel::R);
+        assert_eq!(loc.get_lotated_label(DiceLabel::U), DiceLabel::F);
+        assert_eq!(loc.get_lotated_label(DiceLabel::D), DiceLabel::B);
+        assert_eq!(loc.get_lotated_label(DiceLabel::R), DiceLabel::U);
+        assert_eq!(loc.get_lotated_label(DiceLabel::L), DiceLabel::D);
+
+        let loc = Location::new(DiceLabel::D, DiceLabel::F);
+        assert_eq!(loc.get_lotated_label(DiceLabel::R), DiceLabel::L);
+        assert_eq!(loc.get_lotated_label(DiceLabel::L), DiceLabel::R)
+    }
 }
