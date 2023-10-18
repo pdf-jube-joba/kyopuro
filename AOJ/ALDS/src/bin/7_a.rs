23c4a3dda9b1bd31ec22a96d0bf5c69934d1d8d9
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2023-10-18 15:47:14 +0900

    OK

--- AOJ/ALDS/src/bin/7_a.rs
+++ AOJ/ALDS/src/bin/7_a.rs
@@ -2,82 +2,22 @@ use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use std::collections::VecDeque;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Tree {
     id: usize,
     depth: usize,
-    parent: Option<ToTree>,
-    children: Vec<ToTree>,
+    parent: Option<Rc<RefCell<Tree>>>,
+    children: Vec<Rc<RefCell<Tree>>>,
 }
 
-#[derive(Debug, Clone)]
-struct ToTree(Rc<RefCell<Tree>>);
-
-impl From<Rc<RefCell<Tree>>> for ToTree {
-    fn from(value: Rc<RefCell<Tree>>) -> Self {
-        ToTree(value)
-    }
-}
-
-impl From<Tree> for ToTree {
-    fn from(value: Tree) -> Self {
-        ToTree(Rc::new(RefCell::new(value)))
-    }
-}
-
-impl ToTree {
+impl Tree {
     fn id(self) -> usize {
-        self.0.borrow().id
+        self.id
     }
     fn depth(self) -> usize {
-        self.0.borrow().depth
-    }
-    fn parent(self) -> Option<ToTree> {
-        self.0.borrow().parent.clone()
-    }
-    // fn parent_get(&self) -> &Option<ToTree> {
-    //     self.0.borrow().parent.clone()
-    // }
-    fn children(self) -> Vec<ToTree> {
-        self.0.borrow().children.clone()        
-    }
-    fn children_get(&self) -> Vec<ToTree> {
-        self.0.borrow().children.clone()
-    }
-    fn new(
-        id: usize,
-        children: Vec<ToTree>,
-    ) -> ToTree {
-        let this_tree: ToTree = Tree {
-            id,
-            parent: None,
-            depth: 0,
-            children: children.into_iter().map(|tree| {
-                tree.into()
-            }).collect(),
-        }.into();
-        // for children_tree in this_tree.0.borrow().children {
-        //     children_tree.0.borrow_mut().parent = Some(this_tree.clone());
-        // }
-        this_tree.into()
-    }
-    fn appered_id(&self) -> Vec<usize> {
-        let mut v = vec![self.0.borrow().id];
-        v.extend(self.0.borrow().children.iter().flat_map(|ref_tree| {
-            ref_tree.appered_id()
-        }));
-        v
-    }
-    fn get_ref_from_id(&self, id: usize) -> Option<&ToTree> {
-        if self.0.borrow().id == id {
-            Some(self)
-        } else {
-            None
-        }
+        self.depth
     }
 }
-
-fn get_ref_from_id(tree: ToTree) {}
 
 fn main() {
 }
@@ -87,12 +27,12 @@ mod tests {
     use super::*;
     #[test]
     fn tree_test() {
-        let t1 = ToTree::new(0, vec![]);
-        let t2 = ToTree::new(1, vec![]);
-        let t3 = ToTree::new(2, vec![t1, t2]);
-        eprintln!("{:?}", t3);
-        let mut c = t3.children_get();
-        c = vec![];
-        eprintln!("{:?}", t3);
+        // let t1 = Tree::new(0, vec![]);
+        // let t2 = Tree::new(1, vec![]);
+        // let t3 = Tree::new(2, vec![t1, t2]);
+        // eprintln!("{:?}", t3);
+        // let mut c = t3.children_get();
+        // c = vec![];
+        // eprintln!("{:?}", t3);
     }
 }
