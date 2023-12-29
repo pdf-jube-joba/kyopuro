be44aba551e623795274e6449ab11072df87824b
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2023-12-29 13:58:09 +0900

    OK

--- AoC2023/src/bin/day_18.rs
+++ AoC2023/src/bin/day_18.rs
@@ -141,6 +141,57 @@ fn fill(trench: &mut Vec<Vec<bool>>) {
     }
 }
 
+fn compute_area(input: Vec<(Direction, usize)>) -> usize {
+    let mut min_h: isize = 0;
+    let mut max_h: isize = 0;
+    let mut now_h: isize = 0;
+
+    let mut num_of_integer_point_on_boundary: usize = 0;
+
+    for (d, l) in &input {
+        match d {
+            Direction::U => {
+                now_h -= *l as isize;
+            }
+            Direction::D => {
+                now_h += *l as isize;
+            }
+            _ => {}
+        }
+        min_h = std::cmp::min(min_h, now_h);
+        max_h = std::cmp::max(max_h, now_h);
+        num_of_integer_point_on_boundary += *l;
+    }
+
+    println!("{min_h} {max_h} {num_of_integer_point_on_boundary}");
+
+    let mut now_h: isize = -min_h;
+    let mut area: isize = 0;
+
+    for (d, l) in input {
+        match d {
+            Direction::R => {
+                area += (now_h - min_h) * (l as isize);
+            }
+            Direction::L => {
+                area -= (now_h - min_h) * (l as isize);
+            }
+            Direction::U => {
+                now_h -= l as isize;
+            }
+            Direction::D => {
+                now_h += l as isize;
+            }
+        }
+    }
+    let area = area.unsigned_abs();
+    println!("{}", area);
+    // Pick's theorem
+    let num_of_integer_point_on_interior = area - (num_of_integer_point_on_boundary / 2) + 1;
+
+    num_of_integer_point_on_interior + num_of_integer_point_on_boundary
+}
+
 fn compute_part2(input: Vec<(Direction, usize, String)>) -> usize {
     let input: Vec<_> = input
         .into_iter()
@@ -156,83 +207,7 @@ fn compute_part2(input: Vec<(Direction, usize, String)>) -> usize {
             (dir, len)
         })
         .collect();
-    todo!()
-}
-
-fn compressed_coordinate(v: &Vec<(Direction, usize)>) -> (Vec<usize>, Vec<usize>, Vec<Vec<bool>>) {
-    let mut now: (isize, isize) = (0, 0);
-    let mut min_h = 0;
-    let mut max_h = 0;
-    let mut min_w = 0;
-    let mut max_w = 0;
-    let mut exist_h = vec![0];
-    let mut exist_w = vec![0];
-    for (d, l) in v {
-        match d {
-            Direction::U => {
-                now.0 -= *l as isize;
-                exist_h.push(now.0);
-            }
-            Direction::D => {
-                now.0 += *l as isize;
-                exist_h.push(now.0);
-            }
-            Direction::L => {
-                now.1 -= *l as isize;
-                exist_w.push(now.1);
-            }
-            Direction::R => {
-                now.1 += *l as isize;
-                exist_w.push(now.1);
-            }
-        }
-        min_h = std::cmp::min(min_h, now.0);
-        max_h = std::cmp::max(max_h, now.0);
-        min_w = std::cmp::min(min_w, now.1);
-        max_w = std::cmp::max(max_w, now.1);
-    }
-    let h = (max_h - min_h) as usize + 1;
-    let w = (max_w - min_w) as usize + 1;
-
-    println!("{min_h} {max_h} {min_w} {max_w} {h} {w}");
-
-    let mut exist_h: Vec<_> = exist_h.into_iter().map(|h| (h + (-min_h)) as usize).collect();
-    exist_h.sort();
-
-    let mut exist_w: Vec<_> = exist_w.into_iter().map(|w| (w + (-min_w)) as usize).collect();
-    exist_w.sort();
-
-    let mut now: (usize, usize) = ((-min_h) as usize, (-min_w) as usize);
-    // for (d, l) in input {
-    //     match d {
-    //         Direction::U => {
-    //             for i in 0..*l {
-    //                 trench[now.0 - i][now.1] = true;
-    //             }
-    //             now.0 -= *l;
-    //         }
-    //         Direction::D => {
-    //             for i in 0..*l {
-    //                 trench[now.0 + i][now.1] = true;
-    //             }
-    //             now.0 += *l;
-    //         }
-    //         Direction::L => {
-    //             for i in 0..*l {
-    //                 trench[now.0][now.1 - i] = true;
-    //             }
-    //             now.1 -= *l;
-    //         }
-    //         Direction::R => {
-    //             for i in 0..*l {
-    //                 trench[now.0][now.1 + i] = true;
-    //             }
-    //             now.1 += *l;
-    //         }
-    //     }
-    //     trench[now.0][now.1] = true;
-    // }
-    todo!()
+    compute_area(input)
 }
 
 #[cfg(test)]
@@ -258,5 +233,25 @@ mod tests {
         ];
         let c = compute_part1(order);
         assert_eq!(c, 62);
+    }
+    #[test]
+    fn compute_part2_test() {
+        let order: Vec<(Direction, usize)> = vec![
+            (Direction::R, 6),
+            (Direction::D, 5),
+            (Direction::L, 2),
+            (Direction::D, 2),
+            (Direction::R, 2),
+            (Direction::D, 2),
+            (Direction::L, 5),
+            (Direction::U, 2),
+            (Direction::L, 1),
+            (Direction::U, 2),
+            (Direction::R, 2),
+            (Direction::U, 3),
+            (Direction::L, 2),
+            (Direction::U, 2),
+        ];
+        assert_eq!(compute_area(order), 62);
     }
 }
