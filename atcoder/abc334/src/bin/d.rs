ddd5c507db358e06164b31b7a8a66aa1c2726161
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-01-07 15:52:36 +0900

    OK

--- atcoder/abc334/src/bin/d.rs
+++ atcoder/abc334/src/bin/d.rs
@@ -1,3 +1,47 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, q: usize,
+        mut r: [usize; n],
+        queries: [usize; q],
+    }
+    let res = num_of_sleigh(r, queries);
+    for res in res {
+        println!("{}", res);
+    }
+}
+
+fn num_of_sleigh(mut r: Vec<usize>, queries: Vec<usize>) -> Vec<usize> {
+    r.sort();
+    // sum_r[i \in 0..=r.len()] = if i == 0 then 0 else r[0] + ... + r[i-1]
+    let sum_r = {
+        let mut v = vec![0];
+        let mut sum = 0;
+        for i in 0..r.len() {
+            sum += r[i];
+            v.push(sum);
+        }
+        v
+    };
+
+    // find max m of sum_r[m] = r_0 + .. + r_{m-1} <= x
+    queries 
+        .into_iter()
+        .map(|x| match sum_r.binary_search(&x) {
+            // case: sum_r[m] == x
+            Ok(i) => i,
+            // case: sum_r[m-1] < x < sum_r[m]
+            // m > 0 because sum_r[0] = 0 <= x
+            Err(e) => e - 1,
+        })
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        assert_eq!(num_of_sleigh(vec![2,3], vec![2,3,5]), vec![1,1,2]);
+        assert_eq!(num_of_sleigh(vec![2], vec![1,2,3]), vec![0,1,1]);
+    }
 }
--- atcoder/abc334/src/bin/e.rs
+++ atcoder/abc334/src/bin/e.rs
@@ -1,3 +1,171 @@
+use itertools::iproduct;
+use proconio::marker::Chars;
+use std::collections::VecDeque;
+
+const MOD: usize = 998_244_353;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        h: usize, w: usize,
+        chars: [Chars; h],
+    }
+    let grid: Vec<Vec<bool>> = chars
+        .into_iter()
+        .map(|line| {
+            line.into_iter()
+                .map(|c| match c {
+                    '.' => false,
+                    '#' => true,
+                    _ => unimplemented!(),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let (sum, red) = count(grid);
+
+    let ans = sum * fast_pow(red, MOD - 2, MOD) % MOD;
+
+    println!("{}", ans);
+}
+
+// a^m mod p
+fn fast_pow(mut a: usize, mut m: usize, p: usize) -> usize {
+    let mut res: usize = 1;
+    while m > 0 {
+        if m % 2 == 1 {
+            res = res.checked_mul(a).unwrap() % p;
+        }
+        a = a.checked_mul(a).unwrap() % p;
+        m >>= 1;
+    }
+    res
+}
+
+fn count(grid: Vec<Vec<bool>>) -> (usize, usize) {
+    let (h, w) = (grid.len(), grid[0].len());
+
+    let adj = |(i, j): (usize, usize)| -> Vec<(usize, usize)> {
+        vec![
+            if i > 0 { Some((i - 1, j)) } else { None },
+            if i < h - 1 { Some((i + 1, j)) } else { None },
+            if j > 0 { Some((i, j - 1)) } else { None },
+            if j < w - 1 { Some((i, j + 1)) } else { None },
+        ]
+        .into_iter()
+        .filter_map(|b| b)
+        .collect()
+    };
+
+    let (grid_components, components_num, red_num): (Vec<Vec<Option<usize>>>, usize, usize) = {
+        let mut grid_components: Vec<Vec<Option<usize>>> = vec![vec![None; w]; h];
+        let mut comp_num = 0;
+        let mut red_num = 0;
+
+        for (i, j) in iproduct!(0..h, 0..w) {
+            if grid_components[i][j].is_some() {
+                continue;
+            }
+
+            if !grid[i][j] {
+                red_num += 1;
+                continue;
+            }
+
+            let mut queue: VecDeque<(usize, usize)> = VecDeque::from_iter(vec![(i, j)]);
+
+            while let Some(pt) = queue.pop_front() {
+                if !grid[pt.0][pt.1] || grid_components[pt.0][pt.1].is_some() {
+                    continue;
+                }
+                grid_components[pt.0][pt.1] = Some(comp_num);
+                queue.extend(adj(pt));
+            }
+
+            comp_num += 1;
+        }
+
+        (grid_components, comp_num, red_num)
+    };
+
+    let all_sum: usize = iproduct!(0..h, 0..w)
+        .filter(|&(i, j)| !grid[i][j])
+        .map(|(i, j)| {
+            let mut adj_comp: Vec<usize> = adj((i, j))
+                .into_iter()
+                .filter_map(|(i, j)| grid_components[i][j])
+                .collect();
+            adj_comp.sort();
+            adj_comp.dedup();
+            components_num + 1 - adj_comp.len()
+        })
+        .sum();
+
+    (all_sum, red_num)
+}
+
+#[cfg(test)]
+mod tests {
+    use proconio::source::once::OnceSource;
+
+    use super::*;
+    #[test]
+    fn fast_pow_test() {
+        assert_eq!(fast_pow(1, 0, 7), 1);
+        assert_eq!(fast_pow(2, 0, 7), 1);
+        assert_eq!(fast_pow(5, 0, 7), 1);
+        assert_eq!(fast_pow(1, 1, 7), 1);
+        assert_eq!(fast_pow(1, 5, 7), 1);
+        assert_eq!(fast_pow(2, 1, 7), 2);
+        assert_eq!(fast_pow(2, 2, 7), 4);
+        assert_eq!(fast_pow(2, 3, 7), 1);
+        assert_eq!(fast_pow(2, 4, 7), 2);
+        assert_eq!(fast_pow(3, 1, 7), 3);
+        assert_eq!(fast_pow(3, 2, 7), 2);
+        assert_eq!(fast_pow(3, 3, 7), 6);
+        assert_eq!(fast_pow(3, 4, 7), 4);
+    }
+    fn read_chars(str: &str) -> Vec<Vec<bool>> {
+        let a = OnceSource::from(str);
+        proconio::input! {
+            from a,
+            h: usize, w: usize,
+            chars: [Chars; h],
+        }
+        chars
+            .into_iter()
+            .map(|line| {
+                line.into_iter()
+                    .map(|c| match c {
+                        '.' => false,
+                        '#' => true,
+                        _ => unimplemented!(),
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>()
+    }
+    #[test]
+    fn count_test() {
+        let grid = "1 1\n.";
+        assert_eq!(count(read_chars(grid)), (1, 1));
+
+        let grid = "2 2\n..\n..";
+        assert_eq!(count(read_chars(grid)), (4, 4));
+
+        let grid = "2 2\n#.\n..";
+        assert_eq!(count(read_chars(grid)), (4, 3));
+
+        let grid = "2 2\n##\n..";
+        assert_eq!(count(read_chars(grid)), (2, 2));
+
+        let grid = "2 2\n#.\n.#";
+        assert_eq!(count(read_chars(grid)), (2, 2));
+
+        let grid = "3 3\n#..\n...\n...";
+        assert_eq!(count(read_chars(grid)), (14, 8));
+
+        let grid = "3 3\n#..\n.#.\n...";
+        assert_eq!(count(read_chars(grid)), (15, 7));
+    }
 }
