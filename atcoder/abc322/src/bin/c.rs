db927c95450048a81b65999961f511ecedaaf2ce
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-02-04 11:49:05 +0900

    OK

--- atcoder/abc322/src/bin/c.rs
+++ atcoder/abc322/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::fastout;
+
+#[fastout]
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, m: usize,
+        a: [usize; m],
+    }
+    let mut c = 0;
+    // ci = min { c | a[c] >= i }
+    for i in 1..=n {
+        println!("{}", a[c] - i);
+        if a[c] - i == 0 {
+            c += 1;
+        }
+    }
 }
