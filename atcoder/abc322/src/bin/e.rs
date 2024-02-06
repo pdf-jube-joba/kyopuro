22233b73fea6556a800d59f2c3279dc393082c7f
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-02-06 13:15:11 +0900

    OK

--- atcoder/abc322/src/bin/e.rs
+++ atcoder/abc322/src/bin/e.rs
@@ -1,3 +1,29 @@
+use im_rc::HashMap;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, k: usize, p: usize,
+        c: [(usize, [usize; k+1]); n]
+    }
+}
+
+fn convert_to_base(k: usize, ps: Vec<usize>) -> usize {
+    ps.into_iter()
+        .enumerate()
+        .map(|(i, pi)| pi * k.pow(i as u32))
+        .sum()
+}
+
+fn convert_from_base(k: usize, mut p: usize) -> Vec<usize> {
+    let mut ps = vec![];
+    while p != 0 {
+        ps.push(p % k);
+        p /= k;
+    }
+    ps
+}
+
+fn min_cost(k: usize, p: usize, c: Vec<(usize, Vec<usize>)>) -> Option<usize> {
+    let mut dp: Vec<Vec<usize>> = vec![];
+    todo!()
 }
--- atcoder/abc323/src/bin/e.rs
+++ atcoder/abc323/src/bin/e.rs
@@ -1,3 +1,29 @@
+use ac_library::ModInt998244353 as MInt;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, x: usize,
+        t: [usize; n],
+    }
+    println!("{}", pos(t, x))
+}
+
+fn pos(tis: Vec<usize>, x: usize) -> MInt {
+    let n = tis.len();
+    // p[t] = new music start at t
+    let mut p: Vec<MInt> = vec![MInt::new(1)];
+    for t in 1..=x {
+        let new = tis
+            .iter()
+            .map(|&ti| if t < ti { MInt::new(0) } else { p[t - ti] })
+            .sum::<MInt>()
+            / n;
+        // p[t] = new;
+        p.push(new);
+    }
+    if tis[0] < x + 1 {
+        ((x + 1 - tis[0])..=x).map(|t| p[t]).sum::<MInt>() / n
+    } else {
+        (0..=x).map(|t| p[t]).sum::<MInt>() / n
+    }
 }
