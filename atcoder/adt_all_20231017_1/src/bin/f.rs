5dc0a9a4fc918bc3c9aafee87e9bc3e559a9fe0d
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-02-13 12:28:34 +0900

    OK

--- atcoder/adt_all_20231017_1/src/bin/f.rs
+++ atcoder/adt_all_20231017_1/src/bin/f.rs
@@ -1,3 +1,58 @@
+use std::collections::BTreeSet;
+
+use itertools::Itertools;
+use proconio::marker::Chars;
+enum Move {
+    R,
+    L,
+    U,
+    D,
+}
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        s: Chars,
+    }
+    let s = s
+        .into_iter()
+        .map(|c| match c {
+            'R' => Move::R,
+            'L' => Move::L,
+            'U' => Move::U,
+            'D' => Move::D,
+            _ => unreachable!(),
+        })
+        .collect_vec();
+
+    println!("{}", if f(s) { "Yes" } else { "No" })
+}
+
+fn f(s: Vec<Move>) -> bool {
+    let mut set: BTreeSet<(isize, isize)> = BTreeSet::new();
+    let mut now: (isize, isize) = (0, 0);
+
+    set.insert(now);
+
+    for m in s {
+        match m {
+            Move::R => {
+                now.1 += 1;
+            }
+            Move::L => {
+                now.1 -= 1;
+            }
+            Move::U => {
+                now.0 += 1;
+            }
+            Move::D => {
+                now.0 -= 1;
+            }
+        }
+        if !set.insert(now) {
+            return true;
+        }
+    }
+
+    false
 }
--- atcoder/adt_all_20231017_1/src/bin/g.rs
+++ atcoder/adt_all_20231017_1/src/bin/g.rs
@@ -1,3 +1,58 @@
+use itertools::{iproduct, Itertools};
+
+const LEN: usize = 16;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, m: usize,
+        s: [String; n],
+        t: [String; m],
+    }
+    println!(
+        "{}",
+        if let Some(str) = possible(s, t) {
+            str
+        } else {
+            "-1".to_string()
+        }
+    )
+}
+
+fn gen_str(s: Vec<String>) -> impl Iterator<Item = String> {
+    let n = s.len();
+    let sum_s: usize = s.iter().map(|si| si.len()).sum();
+    let underscore_num = LEN - sum_s - (n - 1);
+    iproduct!(
+        (0..n + underscore_num - 1).combinations(underscore_num), // how to insert `_`
+        (0..n).permutations(n)                                    // order of s
+    )
+    .map(move |(insert, order)| {
+        let mut new_s: Vec<String> = vec![];
+        let mut si = 0;
+        let mut su = 0;
+        for i in 0..n + underscore_num {
+            if su < underscore_num && insert[su] == i {
+                new_s.push("_".to_owned());
+                su += 1;
+            } else {
+                if si < n {
+                    new_s.push(format!("_{}", s[order[si]]));
+                }
+                si += 1;
+            }
+        }
+        assert!(su == underscore_num && si == n);
+        // eprintln!("{:?}", new_s);
+        new_s
+            .into_iter()
+            .collect::<String>()
+            .chars()
+            .skip_while(|&char| char == '_')
+            .collect::<String>()
+    })
+}
+
+fn possible(s: Vec<String>, mut t: Vec<String>) -> Option<String> {
+    t.sort();
+    gen_str(s).find(|si| t.binary_search(si).is_err() && 3 <= si.len() && si.len() <= 16)
 }
