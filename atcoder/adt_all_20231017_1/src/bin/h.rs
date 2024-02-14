29ff6fb23e9736fc8de23453bf884ec95a8226b5
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-02-14 10:50:29 +0900

    OK

--- atcoder/adt_all_20231017_1/src/bin/h.rs
+++ atcoder/adt_all_20231017_1/src/bin/h.rs
@@ -1,3 +1,72 @@
+use itertools::Itertools;
+use proconio::{fastout, marker::Usize1};
+
+#[fastout]
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, q: usize,
+        x: [usize; n],
+        ab: [(Usize1, Usize1); n-1],
+        vk: [(Usize1, Usize1); q],
+    }
+    let k = *vk.iter().map(|(v, k)| k).max().unwrap() + 1;
+    let ans_vec = construct_k_vecs(n, ab, x, k);
+    for (v, k) in vk {
+        println!("{}", ans_vec[v][k]);
+    }
+}
+
+fn construct_k_vecs(
+    n: usize,
+    ab: Vec<(usize, usize)>,
+    x: Vec<usize>,
+    k: usize,
+) -> Vec<Vec<usize>> {
+    // construct ordered graph
+    let edges: Vec<Vec<usize>> = {
+        let mut edges: Vec<Vec<usize>> = vec![vec![]; n];
+        for &(a, b) in &ab {
+            edges[a].push(b);
+            edges[b].push(a);
+        }
+
+        let mut stack = vec![(0, None)];
+        while let Some((next, parent)) = stack.pop() {
+            edges[next].retain(|c| Some(*c) != parent);
+            for &child in &edges[next] {
+                stack.push((child, Some(next)));
+            }
+        }
+        edges
+    };
+
+    let mut ans = vec![vec![]; n];
+
+    dfs(&edges, &x, k, 0, &mut ans);
+
+    ans
+}
+
+fn dfs(
+    edges: &Vec<Vec<usize>>,
+    x: &Vec<usize>,
+    k: usize,
+    vertex: usize,
+    ans: &mut Vec<Vec<usize>>,
+) {
+    for &child in &edges[vertex] {
+        dfs(edges, x, k, child, ans)
+    }
+
+    let mut merged = edges[vertex]
+        .iter()
+        .flat_map(|&child| ans[child].clone())
+        .collect_vec();
+    merged.push(x[vertex]);
+
+    merged.sort();
+    merged.reverse();
+    merged.truncate(k);
+
+    ans[vertex] = merged;
 }
--- atcoder/adt_all_20231017_1/src/bin/i.rs
+++ atcoder/adt_all_20231017_1/src/bin/i.rs
@@ -1,3 +1,26 @@
+use ac_library::ModInt998244353 as ModInt;
+use itertools::Itertools;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        x: Chars,
+    }
+    let x = x
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect_vec();
+    println!("{}", sum_of_d(x));
+}
+
+fn sum_of_d(s: Vec<usize>) -> ModInt {
+    let n = s.len();
+    let mut dp_f: Vec<ModInt> = vec![ModInt::new(1), ModInt::new(s[0])];
+    let mut dp_g: Vec<ModInt> = vec![ModInt::new(1), ModInt::new(1) + dp_f[1]];
+    for i in 2..=n {
+        dp_f.push(ModInt::new(s[i - 1]) * dp_g[i - 1] + ModInt::new(10) * dp_f[i - 1]);
+        dp_g.push(dp_g[i - 1] + dp_f[i]);
+    }
+    dp_f[n]
 }
