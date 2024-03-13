50a697740a67566075b835d0a3424ff1e2b3d7ad
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-03-13 09:59:26 +0900

    OK

--- atcoder/adt_all_20231024_2/src/bin/g.rs
+++ atcoder/adt_all_20231024_2/src/bin/g.rs
@@ -1,7 +1,52 @@
+use std::collections::VecDeque;
+
+use itertools::iproduct;
+use proconio::marker::Usize1;
+
 fn main() {
     proconio::input! {
         n1: usize, n2: usize, m: usize,
-        ab: [(usize, usize); m],
+        ab: [(Usize1, Usize1); m],
     }
-    
+    println!("{}", min_dist_bridge((n1, n2), ab))
+}
+
+fn edgeconv(vertex: usize, edge: Vec<(usize, usize)>) -> Vec<Vec<usize>> {
+    let mut e = vec![vec![]; vertex];
+    for (a, b) in edge {
+        if a == b {
+            continue;
+        }
+        e[a].push(b);
+        e[b].push(a);
+    }
+    e
+}
+
+fn min_dist_bridge((n1, n2): (usize, usize), edge: Vec<(usize, usize)>) -> usize {
+    let n = n1 + n2;
+    let edge = edgeconv(n, edge);
+    let dist1 = min_dist(n, &edge, 0);
+    let dist2 = min_dist(n, &edge, n1 + n2 - 1);
+    let ms = (0..n1).map(|u| dist1[u].unwrap()).max().unwrap();
+    let me = (n1..n).map(|v| dist2[v].unwrap()).max().unwrap();
+    ms + me + 1
+}
+
+fn min_dist(vertex: usize, edge: &Vec<Vec<usize>>, start: usize) -> Vec<Option<usize>> {
+    let mut dist: Vec<Option<usize>> = vec![None; vertex];
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    dist[start] = Some(0);
+    queue.push_back(start);
+
+    while let Some(next) = queue.pop_front() {
+        for &n in &edge[next] {
+            if dist[n].is_none() {
+                dist[n] = Some(dist[next].unwrap() + 1);
+                queue.push_back(n);
+            }
+        }
+    }
+
+    dist
 }
--- atcoder/adt_all_20231024_2/src/bin/h.rs
+++ atcoder/adt_all_20231024_2/src/bin/h.rs
@@ -1,3 +1,36 @@
+use itertools::Itertools;
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, q: usize,
+    }
+    let qs = (0..q)
+        .map(|_| {
+            proconio::input! {
+                t: usize,
+            }
+            if t == 1 {
+                proconio::input! {
+                    u: Usize1, v: Usize1,
+                }
+                Q::Con(u, v)
+            } else {
+                proconio::input! {
+                    u: Usize1,
+                }
+                Q::Del(u)
+            }
+        })
+        .collect_vec();
+}
+
+enum Q {
+    Con(usize, usize),
+    Del(usize),
+}
+
+fn isolation(n: usize, qs: Vec<Q>) -> Vec<usize> {
+
+    todo!()
 }
