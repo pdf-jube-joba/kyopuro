c3cb58ee7fb134f552494f69300edb1816a9c935
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-03-15 09:54:09 +0900

    OK

--- atcoder/adt_all_20231024_2/src/bin/h.rs
+++ atcoder/adt_all_20231024_2/src/bin/h.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 use proconio::marker::Usize1;
 
@@ -23,14 +25,42 @@ fn main() {
             }
         })
         .collect_vec();
+    let ans = isolation(n, qs);
+    println!("{}", ans.iter().join("\n"))
 }
 
+#[derive(Debug, Clone)]
 enum Q {
     Con(usize, usize),
     Del(usize),
 }
 
 fn isolation(n: usize, qs: Vec<Q>) -> Vec<usize> {
+    let mut sets: Vec<HashSet<usize>> = vec![HashSet::new(); n];
+    let mut isolation_set: HashSet<usize> = HashSet::from_iter(0..n);
+    let mut ans = vec![];
 
-    todo!()
+    for q in qs {
+        // eprintln!("q:{q:?}");
+        match q {
+            Q::Con(u, v) => {
+                sets[u].insert(v);
+                sets[v].insert(u);
+                isolation_set.remove(&u);
+                isolation_set.remove(&v);
+            }
+            Q::Del(u) => {
+                for v in sets[u].drain().collect_vec() {
+                    sets[v].remove(&u);
+                    if sets[v].is_empty() {
+                        isolation_set.insert(v);
+                    }
+                }
+                isolation_set.insert(u);
+            }
+        }
+        // eprintln!("a: {sets:?} {isolation_set:?}");
+        ans.push(isolation_set.len());
+    }
+    ans
 }
--- atcoder/adt_all_20231024_2/src/bin/i.rs
+++ atcoder/adt_all_20231024_2/src/bin/i.rs
@@ -1,3 +1,40 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [usize; n],
+        b: [usize; n],
+    }
+    println!("{}", make_bipartite(a, b));
+}
+
+// vertex = (0..=n)
+// edge = { (0, i): weight a[i-1] } for i in 1..=n
+//      or {(i, i + 1 mod (n+1)): weight b[i-1]}) for i in 1..=n
+// return min cost of sum of weight of deleted edges to make bipartite gra@h
+fn make_bipartite(a: Vec<usize>, b: Vec<usize>) -> usize {
+    let n = a.len();
+    // dp[i in 1..=n][j = 0 | 1][k = 0 | 1] = min ..of.. which is restricted to
+    // - vertex is (0..=i)
+    // - vertex 0 is colored 0, vertex 1 is colored j, vertex i is colored k
+    // dp[i + 1][j][k] = min (dp[i][j][k] + b[i-1], dp[i][j][1-k]) + if k = 0 {a[i]} else {0}
+    let mut dp: Vec<((usize, usize), (usize, usize))> = vec![((0, 0), (0, 0)); n + 1];
+    // dp[0], dp[1][0][1] and dp[1][1][0] is nonsense
+    dp[1] = ((a[0], std::usize::MAX), (std::usize::MAX, 0)); // dp[1][0][0] = a[0], dp[1][1][1] = 0
+    for i in 2..=n {
+        let bi = b[i - 2];
+        let ai = a[i - 1];
+        // eprint!("{i} {:?} {} {}", dp[i - 1], bi, ai);
+        dp[i].0 .0 = std::cmp::min(dp[i - 1].0 .0.saturating_add(bi), dp[i - 1].0 .1) + ai;
+        dp[i].0 .1 = std::cmp::min(dp[i - 1].0 .0, dp[i - 1].0 .1.saturating_add(bi));
+        dp[i].1 .0 = std::cmp::min(dp[i - 1].1 .0.saturating_add(bi), dp[i - 1].1 .1) + ai;
+        dp[i].1 .1 = std::cmp::min(dp[i - 1].1 .0, dp[i - 1].1 .1.saturating_add(bi));
+        // eprintln!(" {:?}", dp[i])
+    }
+    let v = vec![
+        dp[n].0 .0 + b[n - 1],
+        dp[n].0 .1,
+        dp[n].1 .0,
+        dp[n].1 .1 + b[n - 1],
+    ];
+    v.into_iter().min().unwrap()
 }
