a8770598adc65d2b35ea205d8c5399d45b862afc
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-03-17 11:38:20 +0900

    OK

--- atcoder/adt_all_20231025_1/src/bin/h.rs
+++ atcoder/adt_all_20231025_1/src/bin/h.rs
@@ -1,3 +1,69 @@
+use std::collections::VecDeque;
+
+use im_rc::HashSet;
+use itertools::Itertools;
+use proconio::{fastout, marker::Usize1};
+
+#[fastout]
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        uv: [(Usize1, Usize1); n - 1],
+    }
+    let ch = to_child_edge(uv, 0);
+    // eprintln!("{ch:?}");
+    for (l, r) in ranges_on_tree(ch, 0) {
+        println!("{} {}", l, r);
+    }
+}
+
+fn to_child_edge(uv: Vec<(usize, usize)>, parent: usize) -> Vec<Vec<usize>> {
+    let n = uv.len() + 1;
+    let mut ch = vec![HashSet::new(); n];
+    for (u, v) in uv {
+        ch[u].insert(v);
+        ch[v].insert(u);
+    }
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+    for &v in &ch[parent] {
+        queue.push_back((v, parent))
+    }
+    while let Some((v, p)) = queue.pop_front() {
+        ch[v].remove(&p);
+        for &c in &ch[v] {
+            queue.push_back((c, v));
+        }
+    }
+    ch.into_iter()
+        .map(|set| set.into_iter().collect())
+        .collect_vec()
+}
+
+fn ranges_on_tree(to_child: Vec<Vec<usize>>, parent: usize) -> Vec<(usize, usize)> {
+    let n = to_child.len();
+    let mut ranges: Vec<Option<(usize, usize)>> = vec![None; n];
+    range_construct(&to_child, &mut ranges, parent, 1);
+    ranges.into_iter().map(|range| range.unwrap()).collect()
+}
+
+// change: ranges[parent] = Some(l, r)
+fn range_construct(
+    to_child: &Vec<Vec<usize>>,
+    ranges: &mut Vec<Option<(usize, usize)>>,
+    parent: usize,
+    start: usize,
+) {
+    if to_child[parent].is_empty() {
+        ranges[parent] = Some((start, start));
+        return;
+    }
+
+    let mut s = start;
+    for &c in &to_child[parent] {
+        range_construct(to_child, ranges, c, s);
+        let (l, r) = ranges[c].unwrap();
+        s = r + 1;
+    }
+
+    ranges[parent] = Some((start, s - 1));
 }
--- atcoder/adt_all_20231025_1/src/bin/i.rs
+++ atcoder/adt_all_20231025_1/src/bin/i.rs
@@ -1,3 +1,76 @@
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [Usize1; n],
+        b: [Usize1; n],
+    }
+    println!(
+        "{}",
+        if simulataneous_swap(a, b) {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
+}
+
+fn simulataneous_swap(a: Vec<usize>, b: Vec<usize>) -> bool {
+    let n = a.len();
+    let mut anum = vec![0; n];
+    let mut bnum = vec![0; n];
+    for i in 0..n {
+        anum[a[i]] += 1;
+        bnum[b[i]] += 1;
+    }
+    if anum != bnum {
+        return false;
+    }
+
+    if anum.iter().any(|num| *num >= 2) {
+        return true;
+    }
+
+    !(is_even_permutation(&a) ^ is_even_permutation(&b))
+}
+
+fn is_even_permutation(a: &Vec<usize>) -> bool {
+    let n = a.len();
+    let mut visited = vec![false; n];
+    let mut cycles: Vec<Vec<usize>> = vec![];
+    for i in 0..n {
+        if visited[i] {
+            continue;
+        }
+        let mut cycle = vec![];
+        let mut current = i;
+        loop {
+            cycle.push(current);
+            visited[current] = true;
+            current = a[current];
+            if current == i {
+                break;
+            }
+        }
+        cycles.push(cycle)
+    }
+    cycles
+        .into_iter()
+        // .inspect(|cycle| eprintln!("{:?} {}", &cycle, (cycle.len() - 1) % 2 == 0))
+        .map(|cycle| (cycle.len() - 1) % 2 == 0)
+        .fold(true, |acc, x| !(acc ^ x))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn evp_test() {
+        assert!(is_even_permutation(&vec![0, 1]));
+        assert!(!is_even_permutation(&vec![1, 0]));
+        assert!(is_even_permutation(&vec![0, 1, 2]));
+        assert!(!is_even_permutation(&vec![2, 1, 0]));
+        assert!(is_even_permutation(&vec![2, 0, 1]));
+    }
 }
