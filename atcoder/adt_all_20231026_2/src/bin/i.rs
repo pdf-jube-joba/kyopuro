777c4179387c9cb375e58c92a1631c734bc78a4a
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-04-16 12:44:26 +0900

    WIP

--- atcoder/adt_all_20231026_2/src/bin/i.rs
+++ atcoder/adt_all_20231026_2/src/bin/i.rs
@@ -5,9 +5,25 @@ fn main() {
         n: usize,
         a: [usize; n],
     }
-
+    println!("{}", make_10_again(a))
 }
 
 fn make_10_again(a: Vec<usize>) -> ModInt {
-    todo!()
+    let n = a.len();
+    let mut dp: Vec<Vec<ModInt>> = vec![vec![ModInt::new(0); 11]; n + 1];
+    dp[0][0] = ModInt::new(1);
+    for i in 0..n {
+        for t in 0..=10 {
+            dp[i + 1][t] = (dp[i][t] * ModInt::new(a[i]))
+                + (1..=a[i])
+                    .take_while(|&dt| t >= dt)
+                    .map(|dt| dp[i][t - dt])
+                    .sum::<ModInt>();
+        }
+    }
+    eprintln!("{dp:?}");
+    let all = a
+        .iter()
+        .fold(ModInt::new(1), |acc, ai| acc * ModInt::new(*ai));
+    dp[n][10] / all
 }
