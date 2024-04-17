78e063fcd2d67bda43156b6492e09ff9a808e9a9
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-04-17 12:13:47 +0900

    OK

--- atcoder/adt_all_20231026_2/src/bin/i.rs
+++ atcoder/adt_all_20231026_2/src/bin/i.rs
@@ -8,22 +8,81 @@ fn main() {
     println!("{}", make_10_again(a))
 }
 
+const M: usize = 10;
+
 fn make_10_again(a: Vec<usize>) -> ModInt {
     let n = a.len();
-    let mut dp: Vec<Vec<ModInt>> = vec![vec![ModInt::new(0); 11]; n + 1];
-    dp[0][0] = ModInt::new(1);
+    let mut dp: Vec<Vec<ModInt>> = vec![vec![ModInt::new(0); 1 << (M + 1)]; n + 1];
+    dp[0][1] = ModInt::new(1);
+    let mask: usize = (1 << (M + 1)) - 1;
     for i in 0..n {
-        for t in 0..=10 {
-            dp[i + 1][t] = (dp[i][t] * ModInt::new(a[i]))
-                + (1..=a[i])
-                    .take_while(|&dt| t >= dt)
-                    .map(|dt| dp[i][t - dt])
-                    .sum::<ModInt>();
+        for s in 0..(1 << (M + 1)) {
+            let tmp = dp[i][s];
+            for t in 1..=M.min(a[i]) {
+                let s2 = (s | (s << t)) & mask;
+                dp[i + 1][s2] += tmp;
+            }
+            if a[i] > M {
+                dp[i + 1][s] += tmp * (a[i] - M);
+            }
         }
     }
-    eprintln!("{dp:?}");
     let all = a
         .iter()
         .fold(ModInt::new(1), |acc, ai| acc * ModInt::new(*ai));
-    dp[n][10] / all
+    let num: ModInt = (0..(1 << M)).map(|s| dp[n][s | (1 << M)]).sum();
+    // eprintln!("{num}");
+    // for i in 0..=n {
+    //     for s in 0..(1 << (M + 1)) {
+    //         if dp[i][s] != ModInt::new(0) {
+    //             eprintln!("{i} {s:#013b} {}", dp[i][s])
+    //         }
+    //     }
+    // }
+    num / all
+}
+
+fn naive(a: Vec<usize>) -> ModInt {
+    let n = a.len();
+    let mask: usize = (1 << (M + 1)) - 1;
+    let all = a
+        .iter()
+        .fold(ModInt::new(1), |acc, ai| acc * ModInt::new(*ai));
+    let num: ModInt = std::iter::successors(Some(vec![1; n]), |s| {
+        let i = (0..n).find(|&i| s[i] < a[i])?;
+        let mut s = s.clone();
+        s[i] += 1;
+        for k in 0..i {
+            s[k] = 1;
+        }
+        Some(s)
+    })
+    .filter(|v| {
+        let mut s = 1;
+        for i in 0..n {
+            if v[i] <= M {
+                s = (s | (s << v[i])) & mask;
+            }
+        }
+        s & (1 << 10) != 0
+    })
+    .inspect(|v| eprintln!("{v:?}"))
+    .count()
+    .into();
+eprintln!("{num}");
+    num / all
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn t() {
+        // let a = vec![11];
+        // assert_eq!(make_10_again(a.clone()), naive(a));
+        // let a = vec![11, 11];
+        // assert_eq!(make_10_again(a.clone()), naive(a));
+        let a = vec![1, 10, 100];
+        assert_eq!(make_10_again(a.clone()), naive(a));
+    }
 }
