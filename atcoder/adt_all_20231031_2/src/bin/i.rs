3954e9a3341bbf054ceef9f46722159f7cde8af1
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-04-21 13:00:47 +0900

    添え字の考え方が変か

--- atcoder/adt_all_20231031_2/src/bin/i.rs
+++ atcoder/adt_all_20231031_2/src/bin/i.rs
@@ -8,10 +8,10 @@ fn main() {
     println!("{}", damage_over_time(h, td));
 }
 
-// sum_{u in l..r} max(a * u, b) // r <= h then result = MAX
+// sum_{u in l..r} max(a * u, b)
 fn integrate((a, b): (usize, usize), (l, r): (usize, usize)) -> usize {
     if a == 0 {
-        return b * (r - l);
+        return b.saturating_mul(r - l);
     }
     let i = b / a;
     let sq = b.saturating_mul(std::cmp::min(r, i).saturating_sub(l));
@@ -24,33 +24,34 @@ fn integrate((a, b): (usize, usize), (l, r): (usize, usize)) -> usize {
 
 // same as min {i in NN | sum_{u in 0..=i} max_{k in 0..n} d[k] * min(u, t[k]) >= H}
 fn damage_over_time(h: usize, td: Vec<(usize, usize)>) -> usize {
+    let mut tdi = (0..td.len()).collect_vec();
+    tdi.sort_by_key(|&i| (td[i].1, std::cmp::Reverse(td[i].0)));
+    tdi.dedup_by_key(|i| td[*i].1);
+    tdi.sort_by_key(|&i| (td[i].0, std::cmp::Reverse(td[i].1)));
+    tdi.dedup_by_key(|i| td[*i].0);
+    // i in tdi => forall j, t[i] = t[j] => d[i] > d[j], d[i] = d[j] => t[i] > t[j]
+    // i in tdi < j in tdi => t[i] < t[j]
+    let td = tdi.into_iter().map(|i| td[i]).collect_vec();
     let n = td.len();
-    let dkref = |k: Option<usize>| -> usize {
-        if let Some(k) = k {
-            td[k].1
-        } else {
-            0
-        }
-    };
-    let tdkref = |k: Option<usize>| -> usize {
-        if let Some(k) = k {
-            td[k].1 * td[k].0
-        } else {
-            0
-        }
-    };
+    eprintln!("{td:?}");
+
+    let dkref = |k: Option<usize>| -> usize { k.map(|k| td[k].1).unwrap_or(0) };
+    let tdkref = |k: Option<usize>| -> usize { k.map(|k| td[k].1 * td[k].0).unwrap_or(0) };
+
     let t2 = {
-        let mut t = vec![0, h];
+        let mut t = vec![0, h, h + 1];
         t.extend(td.iter().map(|td| td.0));
         t.sort();
+        t.dedup();
         t
     };
     let mut i = 0;
     let mut kd = {
         let mut v = (0..n).collect_vec();
-        v.sort_by_key(|i| td[*i].1);
+        v.sort_by_key(|&i| td[i].1);
         v
     };
+    eprintln!("{kd:?}");
     // maxdi = argmax (k in 0..n | t2[i] <= t[k]) |-> d[k]
     // maxfi = argmax (k in 0..n | t[k] < t2[i]) |-> t[k] * d[k]
     let (mut maxdi, mut maxfi): (Option<usize>, Option<usize>) = (kd.pop(), None);
@@ -58,12 +59,14 @@ fn damage_over_time(h: usize, td: Vec<(usize, usize)>) -> usize {
     let mut now_h = 0;
 
     loop {
-        eprintln!("{:?} {:?}", maxdi, maxfi);
         // now_h + sum_{u in t2[i]..t2[i+1]} <= h
         let this_range_sum = integrate((dkref(maxdi), tdkref(maxfi)), (t2[i], t2[i + 1]));
-
-        debug_assert!(
-            (maxdi.is_some() || t2[i + 1] == std::usize::MAX) && (maxfi.is_some() || t2[i] == 0)
+        eprintln!(
+            "{i} {maxdi:?} {:?} {maxfi:?} {:?} [{:?}, {:?}) {this_range_sum:?}",
+            dkref(maxdi),
+            tdkref(maxfi),
+            t2[i],
+            t2[i + 1],
         );
 
         if h <= now_h + this_range_sum {
@@ -74,28 +77,44 @@ fn damage_over_time(h: usize, td: Vec<(usize, usize)>) -> usize {
         i += 1;
         let np = kd.pop();
 
-        if let Some(np) = np {
-            if np >= maxdi.unwrap() {
+        match np {
+            Some(np) => {
+                if np >= maxdi.unwrap() {
+                    if tdkref(maxdi) >= tdkref(maxfi) {
+                        maxfi = maxdi.take();
+                    }
+                    maxdi = Some(np);
+                } else if dkref(Some(np)) >= dkref(maxfi) {
+                    maxfi = Some(np);
+                }
+            }
+            None => {
                 if tdkref(maxdi) >= tdkref(maxfi) {
                     maxfi = maxdi.take();
                 }
-                maxdi = Some(np);
-            } else if dkref(Some(np)) >= dkref(maxfi) {
-                maxfi = Some(np);
-            };
-        } else if tdkref(maxdi) >= tdkref(maxfi) {
-            maxfi = maxdi.take();
+            }
         }
     }
 
+    let from = t2[i];
     let (mut ng, mut ok) = (t2[i], t2[i + 1]);
     while ok - ng > 1 {
         let mid = (ng + ok) / 2;
-        if h <= now_h + integrate((dkref(maxdi), tdkref(maxfi)), (ng, mid)) {
+        if h <= now_h + integrate((dkref(maxdi), tdkref(maxfi)), (from, mid)) {
             ok = mid;
         } else {
             ng = mid;
         }
     }
-    ok
+    ok - 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn t() {
+        let r = damage_over_time(1, vec![(1, 1), (1, 1)]);
+        assert_eq!(r, 1);
+    }
 }
