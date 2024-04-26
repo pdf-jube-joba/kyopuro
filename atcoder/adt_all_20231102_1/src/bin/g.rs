a88be7c66370e74ab978ddb09b76e1e23e003d48
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-04-26 12:10:26 +0900

    OK

--- atcoder/adt_all_20231102_1/src/bin/g.rs
+++ atcoder/adt_all_20231102_1/src/bin/g.rs
@@ -1,6 +1,3 @@
-use num_traits::Pow;
-use permutohedron::factorial;
-
 fn main() {
     proconio::input! {
         k: usize,
@@ -10,7 +7,8 @@ fn main() {
 
 fn factorial_and_multiple(mut k: usize) -> usize {
     let mut prime_num: Vec<(usize, usize)> = vec![];
-    for p in (0_usize..).take_while(move |p| p.pow(2_u32) <= k) {
+    // old(k) = Pi_{(p, a) in prime_num} p^a * now(k)
+    for p in (2_usize..).take_while(move |p| p.pow(2_u32) <= k) {
         let mut n = 0;
         while k % p == 0 {
             k /= p;
@@ -20,5 +18,35 @@ fn factorial_and_multiple(mut k: usize) -> usize {
             prime_num.push((p, n))
         }
     }
-    todo!()
+    if k > 1 {
+        prime_num.push((k, 1));
+    }
+    prime_num
+        .into_iter()
+        .map(|(p, mut a)| f(p, a))
+        .max()
+        .unwrap()
+}
+
+fn f(p: usize, a: usize) -> usize {
+    let mut a: isize = a as isize;
+    let mut n = 0;
+    while a > 0 {
+        n += p;
+        let mut x = n;
+        while x % p == 0 {
+            x /= p;
+            a -= 1;
+        }
+    }
+    n
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn ff() {
+        f(2, 5);
+    }
 }
--- atcoder/adt_all_20231102_1/src/bin/h.rs
+++ atcoder/adt_all_20231102_1/src/bin/h.rs
@@ -1,3 +1,15 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize, m: usize, e: usize,
+        uv: [(usize, usize); e],
+        q: usize,
+        x: [usize; q],
+    }
+    for ans in blackout2((n, m), uv, x) {
+        println!("{ans}");
+    }
+}
+
+fn blackout2((n, m): (usize, usize), uv: Vec<(usize, usize)>, x: Vec<usize>) -> Vec<usize> {
+    todo!()
 }
