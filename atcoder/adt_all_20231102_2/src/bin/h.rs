637a00812c5ae00d44213314f875bef982a92f4b
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-12-16 10:30:56 +0900

    OK

--- atcoder/adt_all_20231102_2/src/bin/h.rs
+++ atcoder/adt_all_20231102_2/src/bin/h.rs
@@ -9,28 +9,34 @@ fn main() {
 }
 
 fn critical_hit(n: usize, p: usize) -> ModInt {
-    let p100 = ModInt::new(p) / ModInt::new(100);
-    let mut p_pow = vec![ModInt::new(1)];
-    let mut nonp_pow = vec![ModInt::new(1)];
-    for _ in 0..n {
-        let p = *p_pow.last().unwrap();
-        p_pow.push(p * p100);
-        let p = *nonp_pow.last().unwrap();
-        nonp_pow.push(p * (ModInt::new(1) - p100));
+    let p = ModInt::new(p) / ModInt::new(100);
+    let nonp = ModInt::new(1) - p;
+    let mut p_pow = vec![ModInt::new(1); n + 1];
+    let mut nonp_pow = vec![ModInt::new(1); n + 1];
+    for i in 0..n {
+        p_pow[i + 1] = p_pow[i] * p;
+        nonp_pow[i + 1] = nonp_pow[i] * nonp;
     }
 
-    let mut fact = vec![ModInt::new(1)];
+    let mut fact = vec![ModInt::new(1); n + 1];
     for i in 1..=n {
-        let fact_prev = *fact.last().unwrap();
-        fact.push(fact_prev * i);
+        fact[i] = fact[i - 1] * i;
     }
-    let comb = |n: usize, k: usize| -> ModInt { fact[n] * fact[k] / fact[n + k] };
 
-    let p1 = |k: usize| -> ModInt { comb(k, n - k) * p_pow[n - k] * nonp_pow[2 * k - n] };
-
-    let p2 =
-        |k: usize| -> ModInt { comb(k - 1, n - k) * p_pow[n - k + 1] * nonp_pow[2 * k - n - 1] };
-
-    (div_ceil(n, 2)..=n).map(|k| p1(k) * k).sum::<ModInt>()
-        + (div_ceil(n + 1, 2)..=n).map(|k| p2(k) * k).sum::<ModInt>()
+    let k1 = (1..=n)
+        .skip_while(|&k| k < n - k)
+        .map(|k| {
+            (fact[k] / (fact[n - k] * fact[2 * k - n]) * p_pow[n - k] * nonp_pow[2 * k - n]) * k
+        })
+        .sum::<ModInt>();
+    let k2 = (1..=n)
+        .skip_while(|&k| (k - 1) < n - k)
+        .map(|k| {
+            (fact[k - 1] / (fact[n - k] * fact[2 * k - n - 1])
+                * p_pow[n - k + 1]
+                * nonp_pow[2 * k - n - 1])
+                * k
+        })
+        .sum::<ModInt>();
+    k1 + k2
 }
